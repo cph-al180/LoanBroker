a0884ab8d2e961eff6945d96f54ed8bb0c44a3b9
AndreasHeindorff <heindorff94@gmail.com>
2017-09-25 19:28:54 +0200

    initial commit

---
+++ src/main/java/dk/cphbusiness/loan_broker/ServiceRunner.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.cphbusiness.loan_broker;
+
+import javax.xml.ws.Endpoint;
+
+public class ServiceRunner {
+    
+	public static void main(String[] args) {
+            String url = "http://localhost:1234/SOAPService";
+            Endpoint.publish(url, new SOAPService());
+            System.out.println("Service started @ " + url);
+	}
+}
+    
