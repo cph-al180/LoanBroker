415ed4ae694394fcb27710266cd64d1184d3cd92
pilgaard <emil.pilgaard@gmail.com>
2017-10-04 08:46:43 +0200

    .

--- src/main/java/controllers/CreditBureauServiceController.java
+++ src/main/java/controllers/CreditBureauServiceController.java
@@ -12,11 +12,23 @@ import models.Consumer;
  * @author Andreas
  */
 public class CreditBureauServiceController {
-    
+
     private String CreditScoreUrl = "http://138.68.85.24:8080/CreditScoreService/CreditScoreService?wsdl";
-    
-    public int GetCreditScore(String SSN){
-        return 0;
+
+    public int GetCreditScore(String SSN) {
+        int result =0;
+        try { // Call Web Service Operation
+            org.bank.credit.web.service.CreditScoreService_Service service = new org.bank.credit.web.service.CreditScoreService_Service();
+            org.bank.credit.web.service.CreditScoreService port = service.getCreditScoreServicePort();
+            // TODO initialize WS operation arguments here
+            java.lang.String ssn = "";
+            // TODO process result here
+            result = port.creditScore(ssn);
+            System.out.println("Result = " + result);
+        } catch (Exception ex) {
+            // TODO handle custom exceptions here
+        }
+        return result;
     }
-    
+
 }
